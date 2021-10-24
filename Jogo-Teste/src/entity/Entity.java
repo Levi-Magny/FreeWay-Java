325d3608538e60a7f29ad511390d07a2d761d836
Levi-Magny <lemmagny@gmail.com>
2021-10-23 23:53:01 -0300

    feat: Adicionado imagem da galinha!

--- Jogo-Teste/src/entity/Entity.java
+++ Jogo-Teste/src/entity/Entity.java
@@ -1,7 +1,9 @@
 package entity;
 
+import java.awt.image.BufferedImage;
 public class Entity { // armazena variáveis que serao usadas no jogador, monstros e NPC.
 	public int x, y;
 	public int speed;
-
+	
+	public BufferedImage look;
 }
--- Jogo-Teste/src/entity/Player.java
+++ Jogo-Teste/src/entity/Player.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -15,12 +19,23 @@ public class Player extends Entity{
 		this.keyH = keyH;
 		
 		setDefaultValues();
+		getPlayerImage();
 	}
 	
 	public void setDefaultValues() {
 		x = 100;
 		y = 100;
 		speed = 4;
+	}
+	
+	public void getPlayerImage() {
+		try {
+			
+			look = ImageIO.read(getClass().getResourceAsStream("/player/Player.png"));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void update() {
@@ -38,8 +53,10 @@ public class Player extends Entity{
 		}
 	}
 	public void draw(Graphics2D g2) {
-		g2.setColor(Color.white);
+		//g2.setColor(Color.white);
+		//g2.fillRect(x, y, gp.tileSize, gp.tileSize); // desenhando um retangulo
 		
-		g2.fillRect(x, y, gp.tileSize, gp.tileSize); // desenhando um retangulo
+		BufferedImage image = look;
+		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 	}
 }
