0f9bbabb90d306656734b8d592337154f2b8a605
Levi-Magny <lemmagny@gmail.com>
2021-10-24 12:50:16 -0300

    feat: Desenhando mapa.

--- Jogo-Teste/src/tile/TileManager.java
+++ Jogo-Teste/src/tile/TileManager.java
@@ -1,6 +1,9 @@
 package tile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -11,12 +14,49 @@ import java.awt.Graphics2D;
 public class TileManager {
 	GamePanel gp;
 	Tile[] tile;
+	int mapTileNum[][];
 	
 	public TileManager(GamePanel gp) {
 		this.gp = gp;
 		
+		mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 		tile = new Tile[10];
+		
 		geTileImage();
+		loadMap();
+	}
+	
+	public void loadMap() {
+		
+		try {
+			
+			InputStream is = getClass().getResourceAsStream("/mapas/Mapa01.txt"); // importa o arquivo de texto
+			BufferedReader br = new BufferedReader(new InputStreamReader(is)); // ler o conteudo do arquivo
+			
+			int col = 0;
+			int row = 0;
+			
+			while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+				String line = br.readLine();
+				
+				while(col < gp.maxScreenCol) {
+					String numbers[] = line.split(" ");
+					
+					int num = Integer.parseInt(numbers[col]);
+					
+					mapTileNum[col][row] = num;
+					col++;
+				}
+				if(col == gp.maxScreenCol) {
+					col = 0;
+					row++;
+				}
+			}
+			
+			br.close();
+		} catch(Exception e) {
+			
+		}
 	}
 	
 	public void geTileImage() {
@@ -24,7 +64,15 @@ public class TileManager {
 		try {
 			
 			tile[0] = new Tile();
-			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/Calcada.png"));
+			tile[1] = new Tile();
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/Rodovia.png"));
+			tile[2] = new Tile();
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/Rodovia_meio.png"));
+			tile[3] = new Tile();
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/Rodovia_cima.png"));
+			tile[4] = new Tile();
+			tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/Rodovia_meio_cima.png"));
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -32,8 +80,26 @@ public class TileManager {
 	}
 	
 	public void draw(Graphics2D g2) {
-		g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
-		g2.drawImage(tile[0].image, 48, 0, gp.tileSize, gp.tileSize, null);
-		g2.drawImage(tile[0].image, 96, 0, gp.tileSize, gp.tileSize, null);
+		
+		int col = 0;
+		int row = 0;
+		int x = 0;
+		int y = 0;
+		
+		while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+			
+			int tileNum = mapTileNum[col][row];
+			
+			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+			col++;
+			x += gp.tileSize;
+			
+			if(col == gp.maxScreenCol) {
+				col = 0;
+				x = 0;
+				row++;
+				y += gp.tileSize;
+			}
+		}
 	}
 }
